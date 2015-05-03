3e37dcc2b07adae3040bad519c534e801e3bea3c
董尚先 <dantesx2012@gmail.com>
2015-05-03 21:11:59 +0800

    失败的尝试

---
+++ SXMoveStatusBar/SXMoveStatusBar/ContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContentViewController.h
+//  SXMoveStatusBar
+//
+//  Created by dsx on 15/4/27.
+//  Copyright (c) 2015年 dsx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentViewController : UIViewController
+
+@end
--- SXMoveStatusBar/SXMoveStatusBar/ViewController.m
+++ SXMoveStatusBar/SXMoveStatusBar/ViewController.m
@@ -10,12 +10,26 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *moveView;
+@property (weak, nonatomic) IBOutlet UIImageView *img;
+
+@property(nonatomic,strong)UIApplication *app;
+@property (nonatomic,assign)BOOL open;
+
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    UIApplication *app = [UIApplication sharedApplication];
+    app.statusBarHidden = YES;
+    self.app = app;
+//    self.open = YES;
+    [self prefersStatusBarHidden];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +37,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+//- (BOOL)prefersStatusBarHidden{
+//    return self.open;
+//}
+
+- (IBAction)btnClick:(id)sender {
+    self.img.image = [self createScreenShot];
+//    self.app.statusBarHidden = YES;
+    [UIView animateWithDuration:0.5 animations:^{
+//        self.moveView.center = CGPointMake(500, self.view.bounds.size.height/2.0);
+    }];
+    
+}
+
+- (UIImage *)createScreenShot
+{
+    UIGraphicsBeginImageContextWithOptions(self.view.frame.size, YES, 0.0);
+    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    return image;
+}
+
 
 @end
